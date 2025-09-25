fefeed10949c8bc485f0a89403cf16992f2d897b
ivanfrasquet <ivanfrasquetmascarell@gmail.com>
2025-09-25 19:28:31 +0200

    Update Main.java

--- bulleti2/src/com/ivanfrasquet/tema01/Main.java
+++ bulleti2/src/com/ivanfrasquet/tema01/Main.java
@@ -12,11 +12,5 @@ public class Main {
         Hashmap hm = new Hashmap("llave", 43);
         System.out.println(hm.getvalue("llave"));
 
-        System.out.println();
-        System.out.println("Act1");
-        System.out.println();
-
-        Act1 a1 = new Act1 ("ficheros", "DNIs.txt");
-        a1.creararxiu();
     }
 }
